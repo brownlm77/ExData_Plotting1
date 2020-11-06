e70607522e7460645db76d4f16a06b4fe12be898
Lawrence Brown <larry@lmbrown.com>
2020-11-05 20:53:19 -0500

    Graph no legend

---
+++ plot3.R
@@ -0,0 +1,94 @@
+#
+#
+
+readData <-function(fname, header=FALSE, nrow=-1) {
+     con <- file(fname, open="rt")
+     on.exit(close(con))
+     df = read.table(con, header=header, nrow=nrow, sep=';', na.strings = '?' ) 
+     return(df)
+}
+
+
+processFile <- function(filepath, nrow = 69520) {
+     con = file(filepath, "rt")
+     on.exit(close(con))
+     
+     # Read Header line 
+     line = readLines(con, n = 1)
+     df = read.table(text = line, header=TRUE, sep=';', na.strings = '?' )
+     header = colnames(df)
+     
+     count <- 1
+     line = readLines(con, n = 1)
+     #while ( !((length(line) == 0) || (count >= nrow) )) {
+     while ( !(length(line) == 0)) {
+          # Only add data if dates are 1/2/2007 and 2/2/2007
+          if (grepl('1/2/2007',line) || grepl('2/2/2007',line)) {
+               dfnew = read.table(text = line, header=FALSE, sep=';', na.strings = '?' ) 
+               df <- rbind(df, dfnew) 
+          }
+          line = readLines(con, n = 1)
+          count = count + 1 
+          # Kick out if past 2/2/2007
+          if (grepl('3/2/2007',line)) {
+               break
+          }
+     }
+     colnames(df) <- header
+     return(df)
+}
+
+plot3 <- function(df) {
+     # Name the plot
+     #png(file = "plot3.png")
+     
+     df$x <- strptime(paste(df$Date, df$Time), "%d/%m/%Y %H:%M:%S")
+     print(head(df))
+     with(df, {plot(x=df$x,
+                   y=df$Sub_metering_1,
+                   ylab='Energy sub metering', 
+                   xlab='',
+                   type = "l",
+                   lty=1
+                   #xlim = c('Thur', 'Fri', 'Sat')
+                   #ylim = c(0, 1200)
+                  )
+               lines(x=df$x,
+                     y=df$Sub_metering_2,
+                     col='red',
+                     lty=2)
+               lines(x=df$x,
+                     y=df$Sub_metering_3,
+                     col='blue',
+                     lty=3)
+               #add a legend in top left corner of chart at (x, y) coordinates = (1, 19)
+               legend( 1,19,
+                      legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+                      col=c("black", "red","blue"),
+                      lty=c(1,2,3), 
+                      ncol=1)
+     
+       }
+     )
+     
+     legend( 1,19,
+             legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+             col=c("black", "red","blue"),
+             #pch=c("-","-","-"),
+             lty=c(1,2,3), 
+             ncol=1)
+     
+          
+     # Save the file.
+     #dev.off()
+}
+
+
+unitTest <- function() {
+     
+     filename <- 'data/household_power_consumption.txt'
+     df = processFile(filename)
+     print(nrow(df))
+     #plot2(df)
+     return(df)
+}
